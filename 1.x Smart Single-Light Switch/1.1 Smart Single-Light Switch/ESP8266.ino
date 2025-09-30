44a3def8ec843333ae1c5df1be6cb374e632c776
Kinga Lechwar <126254663+cyber-kinga@users.noreply.github.com>
2025-09-30 22:42:11 +0200

    Update ESP8266.ino
    Main updates to old code

--- 1.x Smart Single-Light Switch/1.1 Smart Single-Light Switch/ESP8266.ino
+++ 1.x Smart Single-Light Switch/1.1 Smart Single-Light Switch/ESP8266.ino
@@ -1,81 +1,181 @@
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-#include <ESP8266WebServer.h>
-#include <Servo.h>  
-#include <Wire.h>
-#include <BH1750.h>
-#include "PageIndex.h"; 
-#define ServoPort D1  
+/* Documentation: 
+  - https://github.com/esp8266/Arduino
+  - https://docs.arduino.cc/
+      - https://docs.arduino.cc/programming/
+      - https://docs.arduino.cc/libraries/ 
+*/
+#include <ESP8266WebServer.h>  // For setting up a simple web server
+#include <ESP8266WiFi.h>       // For enabling ESP8266 to create a Wi-Fi network
+#include <Servo.h>             // For controlling servomotor
+#include <BH1750.h>            // For controlling light intensity sensor
+#include <Wire.h>              // Library required by BH1750 light intensity sensor
 
-const char* ssid = "Light"; 
-const char* password = "nopassword"; 
+// GPIO pins definition for the servo, LED diode and light intensity sensor pins
+#define SERVO_PIN D1 
+#define LED_PIN D5
+#define SDA_PIN D3 
+#define SCL_PIN D2  
 
-Servo myservo;  
-ESP8266WebServer server(80);  
-BH1750 lightMeter;
-int ledPin = D5;
+// Wi-Fi credentials for Access Point (AP) mode
+const char* WIFI_SSID = "ESP8266-WiFi-Name";         // Set custom ESP8266 Wi-Fi name, for example "Light"  
+const char* WIFI_PASSWORD = "ESP8266-WiFi-Password"; // Set custom and strong ESP8266 Wi-Fi password
 
-void handleRoot() {
-  String s = MAIN_page; 
-  server.send(200, "text/html", s); 
+Servo servoMotor;               // Create a servo object
+ESP8266WebServer webServer(80); // Create a web server object on port 80
+BH1750 lightIntensitySensor;    // Create a light intensity sensor object
+
+// HTML page stored in program memory (PROGMEM) and available at the 192.168.4.1 address
+const char Index[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+      html {
+        font-family: Lato, sans-serif;
+        text-align: center;
+      }
+      h1 {
+        color: #1cb4fa;
+      }
+      .btn {
+        color: white;
+        background-color: #1cb4fa;
+        padding: 20px 55px;
+        text-align: center;
+        text-decoration: none;
+        border: 5px solid transparent; /* żeby podświetlenie nie przesuwało */
+        border: none;
+        cursor: pointer;
+        font-weight: bold;
+      }
+      .active {
+        border: 5px solid #b4e6fd;
+      }
+      .container {
+        margin-top: 50px;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <h1>Control your Single-Light Smart Switch 1.1</h1>
+      <br><br>
+      <button type="button" onclick="sendSignal('on')" class="btn btn-on">On</button>
+      <br><br>
+      <button type="button" onclick="sendSignal('off')" class="btn btn-off">Off</button>   
+    </div>
+    
+    <script>
+      var btnOn = document.querySelector('.btn-on');
+      var btnOff = document.querySelector('.btn-off');
+
+      function sendSignal(action) { 
+          var httpRequest = new XMLHttpRequest();
+          httpRequest.open("GET", "/moveServo?action=" + action, true);   
+          httpRequest.send();
+      }
+
+      function updateLightIntensity() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            var lux = parseFloat(this.responseText);
+            if (lux >= 1000) {
+              btnOn.classList.add('active');
+              btnOff.classList.remove('active');
+            } else {
+              btnOn.classList.remove('active');
+              btnOff.classList.add('active');
+            }
+          }
+        };
+        xhttp.open("GET", "/getLightIntensity", true);
+        xhttp.send();
+      }
+      setInterval(updateLightIntensity, 5000);
+    </script>
+  </body>
+</html>
+)=====";
+
+// Send the HTML page to the client
+void webApp() { 
+  webServer.send(200, "text/html", Index); 
 }
 
-void handleLight() {
-  String state = server.arg("state");
-  if (state == "on") {
-    myservo.write(90);
-    delay(500);
-    myservo.write(60);
-    delay(500);
-    myservo.write(90);
-  } else if (state == "off") {
-    myservo.write(90);
-    delay(500);
-    myservo.write(120);
-    delay(500);
-    myservo.write(90);
+// Comment or delete below function when using powerbank
+// Function to blink the LED diode to indicate signal (action)
+void blinkLed() {
+  digitalWrite(LED_PIN, HIGH); // Turn LED diode on
+  delay(100);                  // Wait 100ms
+  digitalWrite(LED_PIN, LOW);  // Turn LED diode off
+}
+
+// Uncomment the function below when using a powerbank, otherwise - you can delete it.
+// Function to blink the LED diode while keeping it constantly ON
+/*void blinkLed() {
+  digitalWrite(LED_PIN, LOW);  // Turn LED off
+  delay(100);                  // Wait 100ms
+  digitalWrite(LED_PIN, HIGH); // Turn LED back on
+}*/
+
+// Function to control the servomotor based on received signal
+void controlServo() {
+  String signal = webServer.arg("action"); // Get the "action" parameter from the HTTP request
+
+  if (signal == "on") {         // If the signal says "on"
+    blinkLed();                 // Blink LED diode to indicate action
+    servoMotor.write(90);       // Move servo to 90 degrees
+    delay(500);                 // Wait 500ms
+    servoMotor.write(120);      // Move servo to 120 degrees (or 60 degrees)
+    delay(500);                 // Wait 500ms
+    servoMotor.write(90);       // Return to 90 degrees
+  } 
+  else if (signal == "off") {   // If the signal says "off"
+    blinkLed();                 // Blink LED diode to indicate action
+    servoMotor.write(90);       // Move servo to 90 degrees
+    delay(500);                 // Wait 500ms
+    servoMotor.write(60);       // Move servo to 60 degrees (or 120 degrees)
+    delay(500);                 // Wait 500ms
+    servoMotor.write(90);       // Return to 90 degrees
   }
-  server.send(200, "text/plain", "");
+
+  webServer.send(200, "text/plain", "OK"); // Send 200 OK response back to the client
 }
 
+// Function to handle client request for light intensity (lux variable)
 void handleLightIntensity() {
-  float lux = lightMeter.readLightLevel();
-  server.send(200, "text/plain", String(lux));
+  float lux = lightIntensitySensor.readLightLevel();    // Read the current light level from the BH1750 sensor (in lux)
+  webServer.send(200, "text/plain", String(lux));       // Send the lux value back to the web client as plain text
 }
 
+/* The setup() function is called when a sketch starts. 
+  Use it to initialize variables, pin modes, start using libraries, etc. 
+  The setup() function will only run once, after each powerup or reset of the Arduino board. 
+*/
 void setup() {
-  //Serial.begin(115200);
-  //delay(500);
-  myservo.attach(ServoPort); 
-  
-  Wire.begin(D3, D2); 
-  lightMeter.begin();
-  pinMode(ledPin, OUTPUT);
-  
-  WiFi.softAP(ssid, password);
-  IPAddress myIP = WiFi.softAPIP();
-  //Serial.print("AP IP address: ");
-  //Serial.println(myIP);
+  servoMotor.attach(SERVO_PIN); // Attach the servomotor to its pin
+  pinMode(LED_PIN, OUTPUT);     // Set the LED diode pin as an output
 
-  server.on("/", handleRoot); 
-  server.on("/setLight", handleLight);
-  server.on("/getLightIntensity", handleLightIntensity);
+  // Uncomment the line below when using a powerbank, otherwise - you can delete it.
+  // digitalWrite(LED_PIN, HIGH);  // LED diode stays constantly ON at startup
 
-  server.begin();
+  Wire.begin(SDA_PIN, SCL_PIN); // Initialize I2C communication on the defined SDA and SCL pins
+  lightIntensitySensor.begin(); // Initialize the BH1750 light sensor so it’s ready to take readings
+
+  // Start the ESP8266 as an Access Point (AP) mode
+  WiFi.softAP(WIFI_SSID, WIFI_PASSWORD); 
+
+  // Define web server routes
+  webServer.on("/", webApp);                                 // Handle requests using webApp function for the main webpage available at the following address http:/192.168.4.1/
+  webServer.on("/moveServo", controlServo);                  // Handle requests for servo movement available at the following address http:/192.168.4.1/moveServo
+  webServer.on("/getLightIntensity", handleLightIntensity);  // Handle requests for light intensity sensor available at the following address http:/192.168.4.1/getLightIntensity
+  
+  webServer.begin(); // Start the web server 
 }
 
+//The loop() function does precisely what its name suggests, and loops consecutively, allowing your program to change and respond. 
 void loop() {
-  server.handleClient();
-  
-  float lux = lightMeter.readLightLevel();
-  int brightness;
-
-  if (lux < 1000) {
-    brightness = 250; // 50% jasności
-  } else {
-    brightness = 1023; // 100% jasności
-  }
-
-  analogWrite(ledPin, brightness);
-  delay(1000);
+  webServer.handleClient(); // Process incoming client requests
 }
